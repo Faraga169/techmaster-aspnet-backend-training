88304aa5b670cbf59664d1bff26efb127b480062
AhmedFarag <AhmedFaragFekry@gmail.com>
2026-08-13 01:17:24 +0300

    Make Drill02_CalaculatorGrades

---
+++ phase-01-backend-foundations/task-01-csharp-drills/Drills/Drill 02 - Grade Calculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_01_csharp_drills.Drills
+{
+    public static class Drill_02___Grade_Calculator
+    {
+        public static void GradeCalculator(double Grade) {
+
+
+            if (Grade <= 100 && Grade >= 90)
+            {
+
+                Console.WriteLine("Grade A");
+            }
+
+            else if (Grade <= 89 && Grade >= 80)
+            {
+
+                Console.WriteLine("Grade B");
+            }
+
+            else if (Grade <= 79 && Grade >= 70)
+            {
+
+                Console.WriteLine("Grade C");
+            }
+
+            else if (Grade <= 69 && Grade >= 60)
+            {
+
+                Console.WriteLine("Grade D");
+            }
+
+            else  
+            {
+
+                Console.WriteLine("Grade F");
+            }
+
+         
+
+        }
+        }
+    }
+
--- phase-01-backend-foundations/task-01-csharp-drills/Program.cs
+++ phase-01-backend-foundations/task-01-csharp-drills/Program.cs
@@ -8,13 +8,32 @@ namespace task_01_csharp_drills
         {
             bool flag;
             double result;
+            #region Teperature Convertor
+
+            //flag = double.TryParse(Console.ReadLine(), out result);
+            //while (!flag)
+            //{
+            //    Console.WriteLine("Invalid Temperature value, please Enter a valid value: ");
+            //    flag = double.TryParse(Console.ReadLine(), out result);
+            //}
+            //Console.WriteLine($"{result}\u00B0C = {Drill01_TemperatureConverter.ConvertCelsiustoFahrenheit(result):F2}\u00B0F");
+            #endregion
+
+            #region Grade Calaculator
             flag = double.TryParse(Console.ReadLine(), out result);
-            while (!flag) {
-                Console.WriteLine("Invalid Temperature value, please Enter a valid value: ");
+            while (!flag)
+            {
+                Console.WriteLine("Invalid Grade value, please Enter a valid value: ");
                 flag = double.TryParse(Console.ReadLine(), out result);
             }
-            Console.WriteLine($"{result}\u00B0C = {Drill01_TemperatureConverter.ConvertCelsiustoFahrenheit(result):F2}\u00B0F");
-            
+
+            while (result > 100 || result < 0) {
+                Console.WriteLine("Score must be between 0 and 100 ");
+                flag = double.TryParse(Console.ReadLine(), out result);
+            }
+            Drill_02___Grade_Calculator.GradeCalculator(result);
+            #endregion
+
         }
     }
 }

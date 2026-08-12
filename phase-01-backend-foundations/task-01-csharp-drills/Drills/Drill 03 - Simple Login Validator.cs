757d2638481cbabe155cbdf2c8e54fc0cb6ea1b8
AhmedFarag <AhmedFaragFekry@gmail.com>
2026-08-13 01:41:58 +0300

    Make Drill03LoginValidator

---
+++ phase-01-backend-foundations/task-01-csharp-drills/Drills/Drill 03 - Simple Login Validator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_01_csharp_drills.Drills
+{
+    public static class Drill_03___Simple_Login_Validator
+    {
+        public static void LoginValidator(string Username,string Password) {
+
+            const string UserNameAnswer = "Ahmed";
+            const string PasswordAnswer = "Ahmed169";
+
+            int i = 0;
+
+            while (i < 2) {
+                if (String.Equals(Username, UserNameAnswer, StringComparison.OrdinalIgnoreCase) && String.Equals(PasswordAnswer, Password))
+                {
+                    Console.WriteLine("Login successful.");
+                    break;
+                }
+
+                Console.WriteLine("Invalid UserName or Password");
+                Console.WriteLine("Enter Your UserName :");
+                Username = Console.ReadLine() ?? "";
+                Console.WriteLine("Enter Your Password: ");
+                Password = Console.ReadLine() ?? "";
+
+                i++;
+            }
+
+            if (i == 2) {
+                Console.WriteLine("Account locked. Too many failed attempts.");
+            }
+           
+
+             
+            
+
+        }
+    }
+}
--- phase-01-backend-foundations/task-01-csharp-drills/Program.cs
+++ phase-01-backend-foundations/task-01-csharp-drills/Program.cs
@@ -20,18 +20,26 @@ namespace task_01_csharp_drills
             #endregion
 
             #region Grade Calaculator
-            flag = double.TryParse(Console.ReadLine(), out result);
-            while (!flag)
-            {
-                Console.WriteLine("Invalid Grade value, please Enter a valid value: ");
-                flag = double.TryParse(Console.ReadLine(), out result);
-            }
+            //flag = double.TryParse(Console.ReadLine(), out result);
+            //while (!flag)
+            //{
+            //    Console.WriteLine("Invalid Grade value, please Enter a valid value: ");
+            //    flag = double.TryParse(Console.ReadLine(), out result);
+            //}
 
-            while (result > 100 || result < 0) {
-                Console.WriteLine("Score must be between 0 and 100 ");
-                flag = double.TryParse(Console.ReadLine(), out result);
-            }
-            Drill_02___Grade_Calculator.GradeCalculator(result);
+            //while (result > 100 || result < 0) {
+            //    Console.WriteLine("Score must be between 0 and 100 ");
+            //    flag = double.TryParse(Console.ReadLine(), out result);
+            //}
+            //Drill_02___Grade_Calculator.GradeCalculator(result);
+            #endregion
+
+            #region Login
+            Console.WriteLine("Enter Your UserName :");
+            string Username = Console.ReadLine() ?? "";
+            Console.WriteLine("Enter Your Password: ");
+           string Password = Console.ReadLine() ?? "";
+            Drill_03___Simple_Login_Validator.LoginValidator(Username, Password);
             #endregion
 
         }

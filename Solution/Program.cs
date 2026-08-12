6c32e94a9e568406636cc36103a39588ae97001d
AhmedFarag <AhmedFaragFekry@gmail.com>
2026-08-12 23:45:08 +0300

    add running sum solution And Readmefile

---
+++ Solution/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Solution
+{
+    internal class Program
+    {
+        public static int[] RunningSum(int[] nums)
+        {
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    nums[i] += nums[j];
+                }
+            }
+            return nums;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RunningSum([1,2,3,4]));
+           
+    }
+    }
+}
---
+++ Solution01/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Solution01
+{
+    internal class Program
+    {
+        public static int[] RunningSum(int[] nums)
+        {
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    nums[i] += nums[j];
+                }
+            }
+            return nums;
+        }
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4 };
+
+            int[] result = RunningSum(nums);
+
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}

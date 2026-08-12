e949264fab33e5d5ed4c13f896d4c6bc50e39c33
AhmedFarag <AhmedFaragFekry@gmail.com>
2026-08-13 00:59:55 +0300

    Make Drill01_TemperatureSolution

---
+++ phase-01-backend-foundations/task-01-csharp-drills/Drills/Drill01_TemperatureConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_01_csharp_drills.Drills
+{
+    public static class Drill01_TemperatureConverter
+    {
+
+
+
+        public static double ConvertCelsiustoFahrenheit(double temperature) 
+        {
+
+            double result = temperature * 9 / 5 + 32;
+            return result;
+
+
+        }
+           
+          
+    }
+}
--- phase-01-backend-foundations/task-01-csharp-drills/Program.cs
+++ phase-01-backend-foundations/task-01-csharp-drills/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace task_01_csharp_drills
+﻿using task_01_csharp_drills.Drills;
+
+namespace task_01_csharp_drills
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool flag;
+            double result;
+            flag = double.TryParse(Console.ReadLine(), out result);
+            while (!flag) {
+                Console.WriteLine("Invalid Temperature value, please Enter a valid value: ");
+                flag = double.TryParse(Console.ReadLine(), out result);
+            }
+            Console.WriteLine($"{result}\u00B0C = {Drill01_TemperatureConverter.ConvertCelsiustoFahrenheit(result):F2}\u00B0F");
+            
         }
     }
 }

31fd2600fe019867cc2b42299d6101ee8df493a8
AhmedFarag <AhmedFaragFekry@gmail.com>
2026-08-14 17:56:33 +0300

    Make Structure of BankAccountSystem

---
+++ phase-01-backend-foundations/task-02-bank-account-system/Bank Account System/Models/TransactionType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_02_bank_account_system.Bank_Account_System.Models
+{
+    internal class TransactionType
+    {
+
+    }
+}
---
+++ phase-01-backend-foundations/task-02-bank-account-system/Bank Account System/Services/BankService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_02_bank_account_system.Bank_Account_System.Services
+{
+    internal class BankService
+    {
+    }
+}

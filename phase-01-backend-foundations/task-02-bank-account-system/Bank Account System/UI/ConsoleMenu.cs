2dcf5a81dbeaf6c2f4bc180f909efde1ac71cee2
AhmedFarag <AhmedFaragFekry@gmail.com>
2026-08-15 02:15:43 +0300

    Make A Console Menu For UI

--- phase-01-backend-foundations/task-02-bank-account-system/Bank Account System/UI/ConsoleMenu.cs
+++ phase-01-backend-foundations/task-02-bank-account-system/Bank Account System/UI/ConsoleMenu.cs
@@ -4,8 +4,21 @@ using System.Text;
 
 namespace task_02_bank_account_system.Bank_Account_System.UI
 {
-    internal class ConsoleMenu
+    public static class ConsoleMenu
     {
+        public static void ShowMenu() {
 
+            Console.WriteLine("====== TechMaster Bank System ======");
+            Console.WriteLine();
+            Console.WriteLine("1. Create Customer Account");
+            Console.WriteLine("2. Deposit Money");
+            Console.WriteLine("3. Withdraw Money");
+            Console.WriteLine("4. Transfer Money");
+            Console.WriteLine("5. View Account Details");
+            Console.WriteLine("6. View Transaction History");
+            Console.WriteLine("7. View All Accounts");
+            Console.WriteLine("8. Exit");
+            Console.Write("Choose an option: ");
+        }
     }
 }

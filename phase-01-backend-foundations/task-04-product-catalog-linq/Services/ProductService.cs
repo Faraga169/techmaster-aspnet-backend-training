0c9c7e548bea48c5aae3ed72a19efed2fd3fc5e5
AhmedFarag <AhmedFaragFekry@gmail.com>
2026-08-16 20:27:39 +0300

    Make LINQ Query 08 - Count Products per Category in productService

--- phase-01-backend-foundations/task-04-product-catalog-linq/Services/ProductService.cs
+++ phase-01-backend-foundations/task-04-product-catalog-linq/Services/ProductService.cs
@@ -118,6 +118,24 @@ namespace task_04_product_catalog_linq.Services
         }
         #endregion
 
+        #region Count Products per Category
+        public static void CountProductsByCategory()
+        {
 
+            var products = Products.GroupBy(p => p.Category).Select(p => new { Category = p.Key, Count = p.Count() }).ToList();
+            if (products.Count == 0)
+                throw new InvalidOperationException($"No Product Found");
+            foreach (var i in products)
+            {
+
+                Console.WriteLine($"Category {i.Category} has {i.Count} products");
+
+              
+            }
+
+
+
+        }
+        #endregion
     }
 }

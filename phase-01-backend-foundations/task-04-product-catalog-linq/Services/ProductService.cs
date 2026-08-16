0bbc69a533ed01b0a5845ebbb615ad331ecfe278
AhmedFarag <AhmedFaragFekry@gmail.com>
2026-08-16 19:50:05 +0300

    Make LINQ Query 01 - Get All Available Products in ProductService

---
+++ phase-01-backend-foundations/task-04-product-catalog-linq/Services/ProductService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using task_04_product_catalog_linq.Model;
+using task_04_product_catalog_linq.Seeding;
+
+namespace task_04_product_catalog_linq.Services
+{
+    public static class ProductService
+    {
+        public static List<Products> Products { get; private set; } = ProductsSeeding.Seeding();
+
+        #region Get All Available Products
+        public static List<Products> GetAllAvailableProducts() {
+
+           var availableProducts= Products.Where(p => p.IsAvailable).ToList();
+            if (availableProducts.Count == 0)
+                throw new InvalidOperationException("No Products Available");
+
+            return availableProducts;
+
+        }
+        #endregion
+    }
+}

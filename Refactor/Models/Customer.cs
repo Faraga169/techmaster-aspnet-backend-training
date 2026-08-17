c5ed9dba2727ad739db82ee005788d6c62225c93
AhmedFarag <AhmedFaragFekry@gmail.com>
2026-08-17 14:44:34 +0300

    Start At Task05 Make A customerModel

---
+++ Refactor/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Refactor.Models
+{
+    public class Customer
+    {
+        public Customer(string name,CustomerType customerType)
+        {
+            Name = name;
+            CustomerType = customerType;
+        }
+        public string Name { get; private set; } = null!;
+
+        public CustomerType CustomerType { get; private set; }
+    }
+}
---
+++ Refactor/Models/CustomerType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Refactor.Models
+{
+    public enum CustomerType
+    {
+        Regular,
+        Silver,
+        Gold,
+        VIP
+    }
+}

46d6d3d3454fee1f5041e7411da6c98d427e8dcc
AhmedFarag <AhmedFaragFekry@gmail.com>
2026-08-17 16:45:27 +0300

    Make A Order Model

---
+++ Refactor/Models/Order.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Refactor.Models
+{
+    public class Order
+ {
+        //        ProductName
+        //Price
+        //Quantity
+        //Customer
+
+        public Order(string productName,decimal price,int quantity,Customer customer)
+        {
+            ProductName = productName;
+            Price=price;
+            Customer = customer;
+        }
+
+        public string ProductName { get; private set; } = null!;
+
+        public decimal Price { get; private set; }
+
+        public int Quantity { get; private set; }
+
+        public Customer Customer { get; private set; } = null!;
+
+    }
+}

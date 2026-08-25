679becdab576cd455c7828f28f82bf16aafcacf8
AhmedFarag <AhmedFaragFekry@gmail.com>
2026-08-25 20:10:37 +0300

    Make A CustomException

---
+++ phase-02-web-api-basics/task-02-student-management-api/StudentManagementAPI/Exceptions/BusinessException.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentManagementAPI.Exceptions
+{
+    public class BusinessException:Exception
+    {
+        
+        public BusinessException(string message,int statuscode):base(message)
+        {
+            StatusCode = statuscode;
+        }
+
+        public int StatusCode { get; }
+    }
+}

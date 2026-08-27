972541f1ad0a647e49b0553d8389b11528693e2b
AhmedFarag <AhmedFaragFekry@gmail.com>
2026-08-27 03:02:36 +0300

    Make A Models Implementation of BookStoreAPI

--- phase-02-web-api-basics/task-04-book-store-api/Book Store API/Models/Author.cs
+++ phase-02-web-api-basics/task-04-book-store-api/Book Store API/Models/Author.cs
@@ -1,6 +1,22 @@
-﻿namespace Book_Store_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Book_Store_API.Models
 {
     public class Author
     {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Author Name is Required")]
+        [StringLength(100, ErrorMessage = "Author name cannot exceed 100 characters")]
+        public string FullName { get; set; } = null!;
+
+        [Required(ErrorMessage = "Author Country is Required")]
+        [StringLength(100, ErrorMessage = "Country cannot exceed 100 characters")]
+        public string Country { get; set; } = null!;
+
+        [DataType(DataType.Date)]
+        public DateOnly BirthDate { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
--- phase-02-web-api-basics/task-04-book-store-api/Book Store API/Models/Book.cs
+++ phase-02-web-api-basics/task-04-book-store-api/Book Store API/Models/Book.cs
@@ -1,6 +1,34 @@
-﻿namespace Book_Store_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Book_Store_API.Models
 {
     public class Book
     {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Book Title is Required")]
+        public string Title { get; set; } = null!;
+
+        [Required(ErrorMessage = "Book ISBN is Required")]
+        [StringLength(17, ErrorMessage = "ISBN cannot exceed 17 characters")]
+        public string ISBN { get; set; } = null!;
+
+        [Range(1000, 2100, ErrorMessage = "Invalid published year")]
+        public int PublishedYear { get; set; }
+
+        [Range(50,1000000,ErrorMessage = "Price must be between 50 and 1000000")]
+        public decimal Price { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Stock quantity cannot be negative")]
+        public int StockQuantity { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public int AuthorId { get; set; }
+
+        public int CategoryId { get; set; }
+
     }
 }

f8edff222965ab17e2a025dfaeadfd199fa84e2c
AhmedFarag <AhmedFaragFekry@gmail.com>
2026-08-27 03:41:57 +0300

    Make A AuthorController Implementation of BookStoreAPI

--- phase-02-web-api-basics/task-04-book-store-api/Book Store API/Controllers/AuthorsController.cs
+++ phase-02-web-api-basics/task-04-book-store-api/Book Store API/Controllers/AuthorsController.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Book_Store_API.DTOS;
+using Book_Store_API.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Book_Store_API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthorsController : ControllerBase
+    public class AuthorsController(IAuthorService authorService) : ControllerBase
     {
+        [HttpGet]
+        public IActionResult Get() {
+
+            var Authors = authorService.GetAll();
+            return Ok(Authors);
+
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateAuthorRequest createAuthorRequest) { 
+        
+            var Author=authorService.Create(createAuthorRequest);
+            return Ok(Author);
+        }
     }
 }

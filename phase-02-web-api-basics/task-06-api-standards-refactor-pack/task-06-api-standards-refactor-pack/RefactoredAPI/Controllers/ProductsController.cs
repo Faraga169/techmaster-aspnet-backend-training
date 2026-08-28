06ff7ad08cbab7766df1617183bfb1a6bd584607
AhmedFarag <AhmedFaragFekry@gmail.com>
2026-08-28 07:34:55 +0300

    Add ProductController Implementation

--- phase-02-web-api-basics/task-06-api-standards-refactor-pack/task-06-api-standards-refactor-pack/RefactoredAPI/Controllers/ProductsController.cs
+++ phase-02-web-api-basics/task-06-api-standards-refactor-pack/task-06-api-standards-refactor-pack/RefactoredAPI/Controllers/ProductsController.cs
@@ -1,11 +1,43 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using task_06_api_standards_refactor_pack.RefactoredAPI.DTOS;
+using task_06_api_standards_refactor_pack.RefactoredAPI.Services;
 
 namespace task_06_api_standards_refactor_pack.RefactoredAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductsController : ControllerBase
+    public class ProductsController(IProductService productService) : ControllerBase
     {
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            var products = productService.GetAll();
+
+            return Ok(products);
+
+        }
+
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+
+            var product = productService.GetById(id);
+
+            return Ok(product);
+
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateProductRequest createProduct)
+        {
+
+            var productCreate = productService.Create(createProduct);
+
+            return CreatedAtAction(nameof(GetById), new { id = productCreate.Id }, productCreate);
+
+        }
     }
 }

ed1a6fbc79b66a5a516b29c73928164da8b7d64c
AhmedFarag <AhmedFaragFekry@gmail.com>
2026-09-14 21:20:30 +0300

    Make A SpecificationPattern

---
+++ phase-03-real-backend-data-systems/task-03-training-center-database-api/TrainingCenter.DAL/Repositories/Specifications/BaseSpecification.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TrainingCenter.DAL.presistent.Models;
+
+namespace TrainingCenter.DAL.Repositories.Specifications
+{
+    public abstract class BaseSpecification<TEntity> : ISpecification<TEntity> where TEntity:BaseEntity<int>
+    {
+
+        public Expression<Func<TEntity, bool>>? Criteria { get; private set; }
+
+        public List<Expression<Func<TEntity, object>>> Includes { get; } = [];
+
+        public Expression<Func<TEntity, object>>? OrderBy { get; private set; }
+
+        public Expression<Func<TEntity, object>>? OrderByDescending { get; private set; }
+
+
+        protected void AddCriteria(Expression<Func<TEntity, bool>> criteria)
+        {
+            Criteria = criteria;
+        }
+
+        protected void AddInclude(Expression<Func<TEntity, object>> include)
+        {
+            Includes.Add(include);
+        }
+
+        protected void AddOrderBy(Expression<Func<TEntity, object>> orderBy)
+        {
+            OrderBy = orderBy;
+        }
+
+        protected void AddOrderByDescending(Expression<Func<TEntity, object>> orderByDescending)
+        {
+            OrderByDescending = orderByDescending;
+        }
+    }
+}

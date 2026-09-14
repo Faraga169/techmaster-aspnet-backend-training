d30740f82fc570c7f25ccca52af0fa2cebadbdb9
AhmedFarag <AhmedFaragFekry@gmail.com>
2026-09-14 05:06:27 +0300

    Make A  ModelsAndConfigurations in DAL

--- phase-03-real-backend-data-systems/task-03-training-center-database-api/TrainingCenter.DAL/Persistent/Models/Student.cs
+++ phase-03-real-backend-data-systems/task-03-training-center-database-api/TrainingCenter.DAL/Persistent/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,8 +12,10 @@ namespace TrainingCenter.DAL.presistent.Models
     {
         public string FullName { get; set; } = null!;
 
+        [EmailAddress]
         public string Email { get; set; } = null!;
 
+        [Phone]
         public string? PhoneNumber { get; set; }
 
         public bool IsActive{ get; set; }

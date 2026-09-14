28d9ccdded5bf1ba5a201467934afda2083110d6
AhmedFarag <AhmedFaragFekry@gmail.com>
2026-09-14 07:35:40 +0300

    Make A EnrollementRepository

---
+++ phase-03-real-backend-data-systems/task-03-training-center-database-api/TrainingCenter.DAL/Repositories/Implementations/EnrollmentRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TrainingCenter.DAL.Persistent;
+using TrainingCenter.DAL.Persistent.Models;
+using TrainingCenter.DAL.presistent.Models;
+using TrainingCenter.DAL.Repositories.Interfaces;
+
+namespace TrainingCenter.DAL.Repositories.Implementations
+{
+    public class EnrollmentRepository(AppDbContext dbContext) : IEnrollmentRepository
+    {
+
+        public async Task<IEnumerable<Enrollment>> GetAll(EnrollmentStatus? status, int? trackId, int? StudentId, PaymentStatus? PaymentStatus)
+        {
+            var query = dbContext.Enrollmets.AsNoTracking().AsQueryable();
+
+            if (status is not null)
+                query = query.Where(e => e.Status==status.Value);
+            if(trackId is not null)
+                query = query.Where(e => e.TrainingTrackId == trackId);
+            if (StudentId is not null) 
+                query = query.Where(e => e.StudentId == StudentId);
+            if(PaymentStatus is not null)
+                query = query.Where(e => e.Payments.Any(p=>p.Status==PaymentStatus.Value));
+
+            return await query.ToListAsync();
+
+        }
+
+
+        public async Task<Enrollment?> GetById(int id)
+        {
+            var Enrollment = await dbContext.Enrollmets.Include(e=>e.Payments).Include(e=>e.TrainingTrack).Include(e=>e.Student).FirstOrDefaultAsync(e=>e.Id==id);
+            return Enrollment;
+        }
+        
+
+        public async Task<int> Create(Enrollment enrollment)
+        {
+            await dbContext.Enrollmets.AddAsync(enrollment);
+            return await dbContext.SaveChangesAsync();
+        }
+
+        public async Task<int> Update(Enrollment enrollment)
+        {
+            dbContext.Update(enrollment);
+            return await dbContext.SaveChangesAsync();
+        }
+
+
+        public async Task<IEnumerable<Enrollment>> GetEnrollmentsbyStudentId(int studentid)
+        {
+            var EnrollmentsByStudentId = await dbContext.Enrollmets.Where(e => e.StudentId == studentid).ToListAsync();
+            return EnrollmentsByStudentId;
+        }
+
+        public async Task<IEnumerable<Student>> GetStudentsEnrollbyTrackId(int trackid)
+        {
+            var EnrollmentsByStudentId = await dbContext.Students.Where(s=>s.Enrollments.Any(e=>e.TrainingTrackId==trackid)).ToListAsync();
+            return EnrollmentsByStudentId;
+        }
+
+       
+    }
+}
---
+++ phase-03-real-backend-data-systems/task-03-training-center-database-api/TrainingCenter.DAL/Repositories/Interfaces/IEnrollmentRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using TrainingCenter.DAL.Persistent.Models;
+using TrainingCenter.DAL.presistent.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace TrainingCenter.DAL.Repositories.Interfaces
+{
+    public interface IEnrollmentRepository
+    {
+        //GET /api/enrollments Return enrollments with filters: status, trackId, studentId, paymentStatus.
+        //GET /api/enrollments/{id} Return enrollment details with student, track and payments.
+        //POST / api / enrollments Enroll student in track using duplicate and capacity rules.
+        //PUT /api/enrollments/{id
+        //}/ status Change enrollment status using valid transitions.
+        //GET / api / students /{ id}/ enrollments Return student enrollment history.
+        //GET /api/tracks/{id}/ students
+
+
+        public Task<IEnumerable<Enrollment>> GetAll(string? status,int? trackId,int? StudentId,string? PaymentStatus);
+
+        public Task<Enrollment?> GetById(int id);
+
+        public Task<int> Create(Enrollment enrollment);
+
+        public Task<int> Update(Enrollment enrollment);
+
+        public Task<IEnumerable<Enrollment>> GetEnrollmentsbyStudentId(int studentid);
+
+        public Task<IEnumerable<Student>> GetEnrollmentsbyTrackId(int trackid);
+
+
+
+    }
+}

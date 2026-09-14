62d6d573111956472fb082c8d2ff5d36e0155d83
AhmedFarag <AhmedFaragFekry@gmail.com>
2026-09-14 03:29:52 +0300

    Make A baseEntityModel in TrainingCenterDAL

---
+++ phase-03-real-backend-data-systems/task-03-training-center-database-api/TrainingCenter.DAL/Persistent/Models/BaseEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingCenter.DAL.presistent.Models
+{
+    public class BaseEntity<Tkey>
+    {
+        public Tkey Id { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public string? CreatedBy { get; set; } 
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public string? UpdatedBy { get; set; }
+
+        public DateTime? DeletedAt { get; set; }
+
+        public string? DeletedBy { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}

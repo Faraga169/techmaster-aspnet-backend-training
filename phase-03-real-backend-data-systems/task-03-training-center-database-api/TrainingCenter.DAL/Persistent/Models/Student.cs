7e25672030f7001d411176dec5d5dec45a77d427
AhmedFarag <AhmedFaragFekry@gmail.com>
2026-09-14 03:34:06 +0300

    Make A Student Model in DAL

--- phase-03-real-backend-data-systems/task-03-training-center-database-api/TrainingCenter.DAL/Persistent/Models/Student.cs
+++ phase-03-real-backend-data-systems/task-03-training-center-database-api/TrainingCenter.DAL/Persistent/Models/Student.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
 
 namespace TrainingCenter.DAL.presistent.Models
 {
-    internal class Student
+    public class Student:BaseEntity<int>
     {
+        public string FullName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string? PhoneNumber { get; set; }
+
+        public bool IsActive{ get; set; }
     }
 }

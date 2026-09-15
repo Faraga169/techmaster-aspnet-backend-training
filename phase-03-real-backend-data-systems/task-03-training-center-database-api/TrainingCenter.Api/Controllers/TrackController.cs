ed2f3f78b53f20295ca07172f68f2b8382d8736d
AhmedFarag <AhmedFaragFekry@gmail.com>
2026-09-15 09:17:59 +0300

    Make ImplementationofTrackController

---
+++ phase-03-real-backend-data-systems/task-03-training-center-database-api/TrainingCenter.Api/Controllers/TrackController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TrainingCenter.BLL.DTOS;
+using TrainingCenter.BLL.DTOS.Track;
+using TrainingCenter.BLL.Services.Interface;
+using TrainingCenter.DAL.Persistent.Models;
+
+namespace TrainingCenter.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrackController(ITrackService trackService) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll(string? keyword,TrackLevel? level,TrainingStatus? status,int? instructorId)
+        {
+            var result = await trackService.GetAll(keyword,level,status,instructorId);
+
+            return Ok(new ApiResponse<IEnumerable<TrackDTO>>
+            {
+                Success = true,
+                Message = "Tracks retrieved successfully.",
+                Data = result
+            });
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await trackService.GetById(id);
+
+            return Ok(new ApiResponse<TrackDetailsDTO>
+            {
+                Success = true,
+                Message = "Track retrieved successfully.",
+                Data = result
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateTrackDTO dto)
+        {
+            var track = await trackService.Create(dto);
+
+            return CreatedAtAction(
+                nameof(GetById),
+                new { id = track.Id },
+                new ApiResponse<TrackDTO>
+                {
+                    Success = true,
+                    Message = "Track created successfully.",
+                    Data = track
+                });
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, UpdateTrackDTO dto)
+        {
+            dto.Id = id;
+
+            var track = await trackService.Update(dto);
+
+            return Ok(new ApiResponse<TrackDTO>
+            {
+                Success = true,
+                Message = "Track updated successfully.",
+                Data = track
+            });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await trackService.Delete(id);
+
+            return NoContent();
+        }
+    }
+}
